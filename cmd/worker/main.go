c29a7b94ccad17d18acf3870df13cbb344a2aa84
Vitaliy Vasylenko <me@vitalvas.com>
2025-03-03 22:52:06 -0500

    feat: create worker

---
+++ cmd/worker/main.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"slices"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/IBM/sarama"
+	"github.com/gogo/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/prometheus/prompb"
+)
+
+func main() {
+	kafkaConf := sarama.NewConfig()
+	kafkaConf.Version = sarama.V2_8_0_0
+	kafkaConf.Consumer.Return.Errors = true
+	kafkaConf.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	topics := []string{"vmcluster_default"}
+	if row, ok := os.LookupEnv("MIMIC_KAFKA_TOPICS"); ok {
+		topics = strings.Split(row, ",")
+	}
+
+	brokers := []string{"kafka:9092"}
+	if row, ok := os.LookupEnv("MIMIC_KAFKA_BROKERS"); ok {
+		brokers = strings.Split(row, ",")
+	}
+
+	groupID := "prometheus-mimic-worker"
+	if row, ok := os.LookupEnv("MIMIC_KAFKA_GROUP_ID"); ok {
+		groupID = row
+	}
+
+	client, err := sarama.NewConsumerGroup(brokers, groupID, kafkaConf)
+	if err != nil {
+		log.Fatalf("error creating consumer group: %v", err)
+	}
+
+	defer client.Close()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	writeEndpoint := "http://victoriametrics:8428/api/v1/write"
+	if row, ok := os.LookupEnv("MIMIC_WRITE_ENDPOINT"); ok {
+		writeEndpoint = row
+	}
+
+	consumer := NewConsumer(writeEndpoint)
+
+	go func() {
+		defer wg.Done()
+
+		for {
+			if err := client.Consume(ctx, topics, consumer); err != nil {
+				log.Printf("error consuming: %v", err)
+			}
+
+			if ctx.Err() != nil {
+				return
+			}
+
+			consumer.ready = make(chan bool)
+		}
+	}()
+	<-consumer.ready
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+	<-sigterm
+
+	cancel()
+	wg.Wait()
+}
+
+func NewConsumer(writeEndpoint string) *Consumer {
+	return &Consumer{
+		ready: make(chan bool),
+
+		remoteURL: writeEndpoint,
+
+		batchLen:  100_000,
+		batchSize: 30 * 1024 * 1024, // 30MB, no more than maxInsertRequestSize (victoria-metrics)
+		batchTime: 5 * time.Second,
+
+		httpClient: &http.Client{},
+	}
+}
+
+type Consumer struct {
+	ready     chan bool
+	remoteURL string
+
+	batchLen  int
+	batchSize int
+	batchTime time.Duration
+
+	httpClient *http.Client
+}
+
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	close(consumer.ready)
+	return nil
+}
+
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	messages := make([]*sarama.ConsumerMessage, 0, consumer.batchSize)
+	var messagesSize int
+
+	batchTicker := time.NewTicker(consumer.batchTime)
+	defer batchTicker.Stop()
+
+	defer func() {
+		if len(messages) > 0 {
+			consumer.processMessages(messages)
+		}
+	}()
+
+	for {
+		select {
+		case msg := <-claim.Messages():
+			if msg == nil {
+				continue // ignore nil messages
+			}
+
+			messagesSize += len(msg.Value)
+
+			messages = append(messages, msg)
+			session.MarkMessage(msg, "")
+
+			if len(messages) >= consumer.batchLen || messagesSize >= consumer.batchSize {
+				consumer.processMessages(messages)
+				messages = make([]*sarama.ConsumerMessage, 0, consumer.batchSize*2)
+				messagesSize = 0
+
+				batchTicker.Reset(consumer.batchTime)
+			}
+
+		case <-batchTicker.C:
+			if len(messages) > 0 {
+				consumer.processMessages(messages)
+				messages = make([]*sarama.ConsumerMessage, 0, consumer.batchSize*2)
+				messagesSize = 0
+			}
+		}
+	}
+}
+
+func (consumer *Consumer) processMessages(messages []*sarama.ConsumerMessage) {
+	timeSeries := &prompb.WriteRequest{}
+
+	for _, msg := range messages {
+		var ts prompb.TimeSeries
+		if err := proto.Unmarshal(msg.Value, &ts); err != nil {
+			log.Printf("error unmarshaling protobuf: %v", err)
+			return
+		}
+
+		timeSeries.Timeseries = append(timeSeries.Timeseries, ts)
+	}
+
+	messageBytes, err := proto.Marshal(timeSeries)
+	if err != nil {
+		log.Printf("error marshaling protobuf: %v", err)
+		return
+	}
+
+	messageBytesCompressed := snappy.Encode(nil, messageBytes)
+
+	for i := 0; i < 1024; i++ {
+		if err := consumer.sendMessages(messageBytesCompressed); err != nil {
+			log.Printf("error sending messages: %v", err)
+			time.Sleep(1 * time.Second)
+		} else {
+			break
+		}
+	}
+}
+
+func (consumer *Consumer) sendMessages(payload []byte) error {
+	req, err := http.NewRequest(http.MethodPost, consumer.remoteURL, bytes.NewReader(payload))
+	if err != nil {
+		return fmt.Errorf("error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/x-protobuf")
+	req.Header.Set("Content-Encoding", "snappy")
+	req.Header.Set("X-Prometheus-Remote-Write-Version", "0.1.0")
+
+	resp, err := consumer.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending request: %v", err)
+	}
+
+	defer resp.Body.Close()
+
+	if !slices.Contains([]int{http.StatusOK, http.StatusNoContent}, resp.StatusCode) {
+		body, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to send batch. Unexpected status code: %d. Body: %s", resp.StatusCode, string(body))
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/vitalvas/prometheus-mimic
 
-go 1.23
+go 1.24
 
 require (
-	github.com/IBM/sarama v1.45.0
+	github.com/IBM/sarama v1.45.1
 	github.com/gin-gonic/gin v1.10.0
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/snappy v0.0.4
@@ -16,21 +16,20 @@ require (
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/bytedance/sonic v1.11.6 // indirect
-	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/bytedance/sonic v1.12.10 // indirect
+	github.com/bytedance/sonic/loader v0.2.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/cloudwego/base64x v0.1.4 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/eapache/go-resiliency v1.7.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20230731223053-c322873962e3 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/sse v1.0.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.20.0 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/go-playground/validator/v10 v10.25.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/grafana/regexp v0.0.0-20240518133315-a468a5bfb3bc // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
@@ -41,14 +40,14 @@ require (
 	github.com/jcmturner/gokrb5/v8 v8.4.4 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
@@ -58,10 +57,10 @@ require (
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
-	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/protobuf v1.36.4 // indirect
+	golang.org/x/arch v0.14.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
